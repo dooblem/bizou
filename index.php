dcc5699b077b3ac9b019aee52a5ef5f830cbbebf
Marc MAURICE <dooblempub@positon.org>
2011-04-28 17:44:02 +0200

    added a commented line to change the umask of created files

--- index.php
+++ index.php
@@ -69,6 +69,9 @@ function getPreview($imgFile, $maxSize = THUMB_SIZE)
 			return $imgFile;
 		}
 
+		# uncomment this if you need group writable files
+		#umask(0002);
+
 		# create the thumbs directory recursively
 		if (! is_dir(dirname($newImgFile))) mkdir(dirname($newImgFile), 0777, true);
 
