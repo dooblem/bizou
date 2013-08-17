6556d8da10b81cd717af6877b5bec9795c308418
Marc MAURICE <marc-bizou@pub.positon.org>
2013-08-17 22:15:47 +0200

    minor: Changed Tar plugin download label

--- plugins/_disabled/tar/after_content.php
+++ plugins/_disabled/tar/after_content.php
@@ -1,5 +1,5 @@
 <?php if ($GLOBALS['imageFiles'] or $GLOBALS['otherFiles']) { ?>
 <div style="text-align: center; font-size: 0.25cm;">
-<a href="<?= $GLOBALS['rootUrl']."plugins/tar/tar.php".$GLOBALS['simplePath'] ?>">Download all files in this directory</a>
+<a href="<?= $GLOBALS['rootUrl']."plugins/tar/tar.php".$GLOBALS['simplePath'] ?>">Download files (tar archive)</a>
 </div>
 <?php } ?>
