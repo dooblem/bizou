aac71576e1d00d7f72519820146ddda52ab62291
Marc MAURICE <marc-bizou@pub.positon.org>
2015-04-03 19:50:52 +0200

    Handle jpeg orientation in viewer

--- plugins/viewer/template.php
+++ plugins/viewer/template.php
@@ -3,17 +3,18 @@
 <title> <?= IMAGES_DIR.$simpleImagePath ?> </title>
 <style type="text/css">
 html, body {
-height: 100%;
+	height: 100%;
 }
 body {
-margin: 0;
-text-align: center;
-background: black;
-color: white;
+	margin: 0;
+	text-align: center;
+	background: black;
+	color: white;
 }
 #theimage {
-max-width: 100%;
-max-height: 100%;
+	max-width: 100%;
+	max-height: 100%;
+	image-orientation: from-image;
 }
 a {
 	color: white;
